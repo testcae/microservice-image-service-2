4bd3ac007b3047124d2323f558f16f66f6467bd3
ukimiawz <melisa.cecilia@yahoo.com>
2018-01-19 12:56:37 +0100

    Code regeneration/Model synchronization

--- db/image-service-2_create_tables.sql
+++ db/image-service-2_create_tables.sql
@@ -8,7 +8,7 @@
 --
 CREATE TABLE uatTest.tblImage (
   imageId INT(11) ,
-  imageName VARCHAR(255) ,
+  imageUrl VARCHAR(255) ,
   imageUrl VARCHAR(255) ,
 CONSTRAINT imageId_PK PRIMARY KEY (imageId)
  
